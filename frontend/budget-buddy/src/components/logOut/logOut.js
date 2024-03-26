e0ae1e919e93a819e5aa4ae2ae523dce87999728
Lebbl <72892765+Lebbl@users.noreply.github.com>
2024-03-25 23:00:28 -0600

    Made Login work (scuffed)

--- frontend/budget-buddy/src/components/logOut/logOut.js
+++ frontend/budget-buddy/src/components/logOut/logOut.js
@@ -3,15 +3,17 @@ import styled from "styled-components";
 import { InnerLayout } from "../../styles/pageLayouts";
 import Button from "../button/button";
 import { noSign, yesSign } from "../../utils/icons";
+import { GetMainContext } from "../../context/mainContext";
 
 function LogOut({setCurrent}){
+    const{setLoggedIn} = GetMainContext();
     return(
         <StyledLogOut>
             <InnerLayout>
                 <h1 className="logOut">Log Out?</h1>
                 <div className="buttons">
                     <Button name={"No"} icon={noSign} buttonPad={'1rem'} buttonRadius={'10px'} buttonBackground={'white'} color={'var(--primaryColor)'} iColor={'white'} onClick={() => setCurrent(3)}/>
-                    <Button name={"Yes"} icon={yesSign} buttonPad={'1rem'} buttonRadius={'10px'} buttonBackground={'white'} color={'var(--primaryColor)'} iColor={'white'} onClick={() => setCurrent(5)}/>
+                    <Button name={"Yes"} icon={yesSign} buttonPad={'1rem'} buttonRadius={'10px'} buttonBackground={'white'} color={'var(--primaryColor)'} iColor={'white'} onClick={() => {setLoggedIn(false); setCurrent(5)}}/>
                 </div>
             </InnerLayout>
         </StyledLogOut>
